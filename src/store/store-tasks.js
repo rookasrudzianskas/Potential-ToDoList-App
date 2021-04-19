6d654ee8e5df41828252b7d6ce49435fd3e6c43e
Rokas <rookas.rudzenskas@gmail.com>
2021-04-19 11:13:45 +0300

    Rokas

---
+++ src/store/store-tasks.js
@@ -0,0 +1,45 @@
+const state = {
+  tasks: [
+    {
+      id: 1,
+      name: 'Go to shop',
+      completed: false,
+      dueTime: '14:30',
+      dueDate: '2020/05/12',
+    },
+    {
+      id: 2,
+      name: 'Go to home',
+      completed: false,
+      dueTime: '14:30',
+      dueDate: '2020/05/15',
+    },
+    {
+      id: 3,
+      name: 'Go to school',
+      completed: false,
+      dueTime: '14:30',
+      dueDate: '2020/05/14',
+    }
+  ]
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
